080e3f664a2f6fcc5fc5b6f25497109826ae6880
Kevin <kev.yuan01@gmail.com>
2024-10-06 15:12:20 -0700

    removed unused package

--- CSC305_Lab_2/src/taskTwo/Driver.java
+++ CSC305_Lab_2/src/taskTwo/Driver.java
@@ -1,7 +1,5 @@
 package taskTwo;
 
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -11,7 +9,7 @@ import java.util.Arrays;
 
 public class Driver {
 
-    public static void main(String[] args) throws JsonProcessingException, IOException {
+    public static void main(String[] args) throws IOException {
        final ObjectMapper mapper = new ObjectMapper();
        ArrayList<String> jsonFile = new ArrayList<>(Arrays.asList("CSC305_Lab_2/input1.json", "CSC305_Lab_2/input2.json", "CSC305_Lab_2/input3.json"));
 
